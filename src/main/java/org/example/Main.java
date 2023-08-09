d5bf58ce58916991a15a187d66388e196b516930
SamMacbokAir <w304714766@gmail.com>
2023-08-09 15:59:05 +1000

    Init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.json.simple.JSONObject;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        JSONObject data = new JSONObject();
+    }
+}
