1b07c338c5584fa7e3e12f32b4c371727d6f9a0c
SamMacbokAir <w304714766@gmail.com>
2023-08-17 22:11:47 +1000

    Assignment 1.Dictionary

--- src/main/java/Server/Dictionary.java
+++ src/main/java/Server/Dictionary.java
@@ -1,13 +1,69 @@
 package Server;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
 import java.util.HashMap;
 
 public class Dictionary {
     private String path = "Dictionary.json";
-    private HashMap<String, String> dictMap;
+    private JSONObject dictionaryJSON;
+    public Dictionary(String path) {
+        this.path = path;
+        initialization();
+    }
+    private void initialization() {
+        JSONParser parser = new JSONParser();
+        try {
+            FileReader reader = new FileReader(path);
+            // Parse the JSON file
+            dictionaryJSON = (JSONObject) parser.parse(reader);
+            reader.close();
+        } catch (IOException e) {
+            //File not found
+            e.printStackTrace();
+        } catch (ParseException e) {
+            //ParseException
+            e.printStackTrace();
+        }
+    }
+    public synchronized boolean isWordExist(String wordName) {
+        return dictionaryJSON.containsKey(wordName);
+    }
+    public synchronized String query(String wordName) {
+        return dictionaryJSON.get(wordName).toString();
+    }
+    public synchronized boolean add(String wordName, String wordMeaning) {
+        if (dictionaryJSON.containsKey(wordName)) {
+            return false;
+        } else {
+            dictionaryJSON.put(wordName, wordMeaning);
+            return true;
+        }
+    }
 
+    public synchronized boolean remove(String wordName) {
+        if (dictionaryJSON.containsKey(wordName)) {
+            dictionaryJSON.remove(wordName);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    public synchronized void updateLocalJSON() {
+        try {
+            FileWriter writer = new FileWriter(path);
+            writer.write(dictionaryJSON.toJSONString());
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
     public String getPath() {
         return path;
     }
@@ -16,11 +72,12 @@ public class Dictionary {
         this.path = path;
     }
 
-    public HashMap<String, String> getDictMap() {
-        return dictMap;
+    public JSONObject getDictionaryJSON() {
+        return dictionaryJSON;
     }
 
-    public void setDictMap(HashMap<String, String> dictMap) {
-        this.dictMap = dictMap;
+    public void setDictionaryJSON(JSONObject dictionaryJSON) {
+        this.dictionaryJSON = dictionaryJSON;
     }
 }
+
---
+++ src/main/java/Test1.java
@@ -0,0 +1,8 @@
+import Server.Dictionary;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Dictionary dictionary = new Dictionary("src/Dictionary.json");
+
+    }
+}
